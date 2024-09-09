a60cd3a02877774c5aee48190591145019665edb
2106123 <2106123@itg.ac.id>
2024-09-09 06:56:39 +0000

    ubah gambar dan membuat register_screen yang baru

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,32 +11,67 @@ class LoginScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(50),
         children: [
-          Image.asset('assets/images/logoMonja.png'),
-          _formInput(),
           const SizedBox(height: 50),
-          buttonLogin()
+          Image.asset(
+            alignment: Alignment.center,
+            'assets/images/logoMonja.png',
+            width: 150,
+            height: 150,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          _formInput(),
+          Container(
+            alignment: Alignment.topRight,
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot Password?'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          buttonLogin(),
+          createAccount(context),
         ],
+      ),
+    );
+  }
+
+  Center createAccount(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const RegisterScreen(),
+            ),
+          );
+        },
+        child: const Text('Create Account'),
       ),
     );
   }
 
   ElevatedButton buttonLogin() {
     return ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: const Text(
-            'Login',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold),
-          ),
-        );
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: const Text(
+        'Login',
+        style: TextStyle(
+            color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 
   Column _formInput() {
@@ -44,14 +80,21 @@ class LoginScreen extends StatelessWidget {
       children: [
         TextField(
           decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              )),
+            contentPadding: const EdgeInsets.all(15),
+            filled: true,
+            fillColor: Colors.white,
+            labelText: 'Email',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
         const SizedBox(height: 20),
         TextField(
           decoration: InputDecoration(
+            contentPadding: const EdgeInsets.all(15),
+            filled: true,
+            fillColor: Colors.white,
             labelText: 'Password',
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/login_screen.dart'; // Assuming LoginScreen exists
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(50),
+        children: [
+          const SizedBox(height: 30),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Nama Lengkap',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Alamat',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Nomor Telepon',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Company',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(15),
+              filled: true,
+              fillColor: Colors.white,
+              labelText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Implement registration logic here
+              // For example, you can validate input, send data to a server, etc.
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            child: const Text(
+              'Register',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
