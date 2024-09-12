85049b38c4ff028ec35e8b5e3c56a7d8e7727db7
2106123 <2106123@itg.ac.id>
2024-09-12 02:34:22 +0000

    debug things

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  String nama;
+  String alamat;
+  String email;
+  String password;
+  String noTelpon;
+  String company;
+
+  User({
+    required this.nama,
+    required this.alamat,
+    required this.email,
+    required this.password,
+    required this.noTelpon,
+    required this.company,
+  });
+}
